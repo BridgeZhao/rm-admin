db38bc9129f3903c0d33d517247c9e8c1fc4e41e
赵梁 <zhaoliang@zhaoliangdeMacBook-Pro.local>
2019-06-24 16:59:10 +0800

    新增功能以及部分样式修改

--- src/views/game/miniGame.vue
+++ src/views/game/miniGame.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="mini-game">
+  <div class="mini_game">
     <ul  class="data-list game-list">
       <li >
-        <div  class="name-warp"><span  class="name">颜值打分</span> <button
-             class="delete-icon"></button></div>
+        <div  class="name-warp"><span  class="name">颜值打分</span>
+         <svg-icon icon-class="trash" class="delete-icon" />
+         </div>
         <div  class="img" style="background-image: url('http://172.16.2.118/media/5d0b47c4e7f8b535833263.jpg');"></div>
       </li>
-      <li  class="add"><button  class="add-btn"></button> <span 
-          class="font-red">添加游戏</span></li>
+      <li  class="add">
+        <svg-icon icon-class="plus" class="svg-plus"></svg-icon>
+         <span class="font-red">添加游戏</span>
+      </li>
     </ul>
     <div  class="cs-dialog dark1" style="display: none;">
       <div class="add-game" data-v-3248c00a="">
@@ -23,9 +26,12 @@
             </li>
             <li>
               <div class="item-title">上传预览图片</div>
-              <div  class="cs-upload-warpper"><input  type="file"
-                  accept="*" class="file-input"> <input  type="text" readonly="readonly" class="input">
-                <span  class="upload-icon"></span></div>
+              <div  class="cs-upload-warpper">
+                <input  type="file" accept="*" class="file-input">
+                <input  type="text" readonly="readonly" class="input">
+                <span  class="upload-icon"></span>
+                
+              </div>
             </li>
           </ul>
           <div class="preview-panel"></div>
@@ -42,6 +48,6 @@
     name: 'miniGame'
   }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
   @import "../../styles/game.scss";
 </style>
--- src/views/game/storeContent.vue
+++ src/views/game/storeContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id='store_content'>
     <div class="top-bar">
       <ul class="feature-tab-list">
         <li class="" :class="{ selected: gameTab }" @click='tabclick("first")'>店内导航</li>
@@ -8,39 +8,9 @@
     </div>
     <div v-show='gameTab' class="first_item">
       <div class="filter-warp">
-        <div class="drop-down area-drop-down dark1">
-          <!-- 	<div class="main"><span title="测试" class="text-ellipsis">测试</span> <span class="arrow down"></span></div>
-					<ul class="popup-list" style="display: none;">
-						<li class="selected"><span title="测试" class="text-ellipsis">测试</span>
-						  
-						</li>
-						<li class=""><span title="测试区" class="text-ellipsis">测试区</span>
-						  
-						</li>
-						<li class=""><span title="测试123" class="text-ellipsis">测试123</span>
-						  
-						</li>
-						<li class=""><span title="25" class="text-ellipsis">25</span>
-						  
-						</li>
-						<li class=""><span title="35" class="text-ellipsis">35</span>
-						  
-						</li>
-						<li class=""><span title="36" class="text-ellipsis">36</span>
-						  
-						</li>
-						<li class=""><span title="37" class="text-ellipsis">37</span>
-						  
-						</li>
-						<li class=""><span title="41" class="text-ellipsis">41</span>
-						  
-						</li>
-						<li class=""><span title="展厅区" class="text-ellipsis">展厅区</span>
-						  
-						</li>
-					</ul> -->
+        <div class="drop-down area-drop-down">
           <template>
-            <el-select v-model="value" placeholder="请选择">
+            <el-select v-model="value" placeholder="请选择"  v-on:change="indexSelect" >
               <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
               </el-option>
             </el-select>
@@ -50,39 +20,69 @@
         <button class="gradient">发布</button>
       </div>
       <ul class="data-list">
-        <li v-for='(item, key) in listdata' :key=key>
-          <div class="name-warp"><span class="name">{{item.name}}</span> <svg-icon icon-class="trash" class="delete-icon" /></div>
+        <li v-for='(item, key) in listdata' :key='key'>
+          <div class="name-warp"><span class="name">{{item.name}}</span> <svg-icon icon-class="trash" class="delete-icon" @click="deleteRegion(key)" /></div>
           <div class="desc-warp"><span>上次更新时间</span> <span>{{item.time}}</span></div>
           <ul class="preview-warp">
             <li>
               <h1 class="title">区域缩略图</h1>
-              <div class="img" :style="{backgroundImage: 'url(' + item.imgThumbnail + ')'}"></div>
-              <div  class="cs-upload-warpper file-upload">
-                <input  type="file" accept="*" class="file-input"> 
-                <input  type="text" readonly="readonly" class="input">
-                <svg-icon icon-class="upload" class="upload-icon" />
-              </div> 
+              <div class="img">
+                <img :src="item.imgThumbnail" alt="">
+              </div>
+              <el-upload
+                class="upload-img"
+                action=""
+                ref="upload"
+                name="imgThumbnail"
+                :auto-upload="false"
+                :show-file-list="false"
+                :on-change="handlePreview">
+                <div class="text-left">
+                  <el-button type="primary" class="file-upload" size="small" @click="handleClickFile('imgThumbnail' ,key)">
+                    <svg-icon icon-class="upload" class="upload-icon svg-icon" />
+                  </el-button>
+                </div>
+              </el-upload>
               <button  class="btns handle-btn default">更新</button>
             </li>
             <li>
               <h1 class="title">区域导航图</h1>
-              <div class="img" :style="{backgroundImage: 'url(' + item.imgNavigation + ')'}"></div>
-              <div  class="cs-upload-warpper file-upload">
+              <div class="img">
+                  <img :src="item.imgNavigation" alt="">
+              </div>
+              <!-- <div  class="cs-upload-warpper file-upload">
                 <input  type="file" accept="*" class="file-input"> 
                 <input  type="text" readonly="readonly" class="input">
-                <svg-icon icon-class="upload" class="upload-icon" />
-              </div>
+                <svg-icon icon-class="upload" class="upload-icon svg-icon" />
+              </div> -->
+              <el-upload
+                class="upload-img"
+                action=""
+                ref="upload"
+                name="imgNavigation"
+                :auto-upload="false"
+                :show-file-list="false"
+                :on-change="handlePreview">
+                <div class="text-left">
+                  <el-button type="primary" class="file-upload" size="small" @click="handleClickFile('imgNavigation' ,key)">
+                    <svg-icon icon-class="upload" class="upload-icon svg-icon" />
+                  </el-button>
+                </div>
+              </el-upload>
               <button  class="btns handle-btn default">更新</button>
             </li>
           </ul>
         </li>
       </ul>
     </div>
+    <!-- 小游戏 -->
     <div v-show='!gameTab' class="second_item">
       <ul  class="data-list game-list">
         <li  class="game-list-li" draggable="false">
-          <div  class="name-warp"><span  class="name">贴纸游戏</span> <button
-               class="icon edit"></button></div>
+          <div  class="name-warp">
+            <span  class="name">贴纸游戏</span>
+            <svg-icon icon-class="edit" class="svg-icon"></svg-icon>
+          </div>
           <div  class="img" style="background-image: url('http://172.16.2.118/media/5d0b47c4e7f8b535833263.jpg');"></div>
           <button class="btns handle-btn mr-top default">取消关联</button>
         </li>
@@ -111,7 +111,12 @@
           time: '2019-06-06',
           imgThumbnail:'http://172.16.2.118/media/5cf8bd48e042e128180005.png',
           imgNavigation:'http://172.16.2.118/media/5cf8bd4f920e8885753787.png'
-        }]
+        }],
+        fileReader: new FileReader(),
+        changeImgBase64 : false,
+        listdataImg : undefined,
+        listdataIn : undefined,
+        nowId : ''
       }
     },
     methods: {
@@ -119,27 +124,70 @@
         this.gameTab = tabClickVal == 'second' ? false : true;
       },
       addRegion () {
-         const _listdata = [{
+         const _listdata = {
            id:'',
            name: '',
            time: '',
            imgThumbnail:'',
            imgNavigation:''
-         }];
+         };
+         _listdata.name = this.nowId.label
          let thisListdata = this.listdata
          let lastListdata = thisListdata[thisListdata.length - 1]
-          if(lastListdata.id == '' || lastListdata.id == undefined){
-            this.$message('该区域已存在，请勿重复添加');
+          // if(lastListdata.id == '' || lastListdata.id == undefined){
+          if(lastListdata.id === undefined  || lastListdata.name === this.nowId.label){
+            this.$message({
+              message: '该区域已存在，请勿重复添加',
+              type: 'warning'
+            });
             return false
           }
          this.listdata.push(_listdata)
       },
-      deleteRegion () {
-        
+      deleteRegion (key) {
+        this.listdata.splice(key,1)
+        this.$message({
+          message: '删除成功',
+          type: 'success'
+        });
+      },
+      handlePreview(file) {
+        this.changeImgBase64=true
+        let mun = this.listdataIn
+        let listdataImg = this.listdataImg
+        const fileName = file.name
+        const isLimt = file.size / 1024 / 1024 < 1
+        const regex = /(.jpg|.jpeg|.png)$/
+        if (regex.test(fileName.toLowerCase())) {
+          if (isLimt) {
+            this.fileReader.readAsDataURL(file.raw)
+            this.fileReader.onload = (res) => {
+              listdataImg === 'imgThumbnail' ? this.listdata[mun].imgThumbnail= res.currentTarget.result : this.listdata[mun].imgNavigation = res.currentTarget.result
+            }
+          }else{
+            this.$message.error('上传图片不能超过1MB')
+          }
+        }else{
+          this.$message.error('只能上传jpg或png格式')
+        }
+      },
+      handleClickFile(type, key) {
+        this.listdataIn = key
+        this.listdataImg = type
+      },
+      indexSelect(){
+        let thisVal = this.value
+        this.nowId = this.options.find(function(item){
+          return item.value === thisVal
+        })
+        console.log(this.nowId)
+         
       }
+    },
+    watch: {
     }
   }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
   @import "../../styles/game.scss";
 </style>
